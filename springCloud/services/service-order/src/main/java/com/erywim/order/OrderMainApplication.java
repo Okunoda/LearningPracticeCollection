beb05d6a489b043ecc49d20fb5f871631abf942b
erywim <erywim@163.com>
2025-10-04 11:43:24 +0800

    class5. 使用NacosConfigManager 监听配置文件的变化

--- springCloud/services/service-order/src/main/java/com/erywim/order/OrderMainApplication.java
+++ springCloud/services/service-order/src/main/java/com/erywim/order/OrderMainApplication.java
@@ -1,12 +1,43 @@
 package com.erywim.order;
 
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager){
+        return args -> {
+            System.out.println("OrderMainApplication started");
+
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.yml", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(3);
+                }
+
+                @Override
+                public void receiveConfigInfo(String s) {
+                    System.out.println("收到的变更配置为: " + s);
+                    System.out.println("其他操作.....");
+                }
+            });
+        };
     }
 }
