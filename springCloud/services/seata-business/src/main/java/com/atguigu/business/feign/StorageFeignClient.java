6a3fefeba1b7072c55f3a34cd78c0e9165f35554
erywim <erywim@163.com>
2025-10-06 00:00:56 +0800

    class18. seata测试打通分布式调用链路

---
+++ springCloud/services/seata-business/src/main/java/com/atguigu/business/feign/StorageFeignClient.java
@@ -0,0 +1,15 @@
+package com.atguigu.business.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value="seata-storage")
+public interface StorageFeignClient {
+
+    @GetMapping("/deduct")
+    public String deduct(@RequestParam("commodityCode") String commodityCode,
+                         @RequestParam("count") Integer count);
+
+}
--- springCloud/services/seata-order/src/main/java/com/atguigu/order/SeataOrderMainApplication.java
+++ springCloud/services/seata-order/src/main/java/com/atguigu/order/SeataOrderMainApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @MapperScan("com.atguigu.order.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.atguigu.order.feign")
 public class SeataOrderMainApplication {
 
     public static void main(String[] args) {
